071f2de0b7bbe0d6d2c802e6eb059d5bcebb4ec9
yangxiaofeng <xiaofeng.yang.sg@gmail.com>
2025-03-19 07:00:08 +0000

    Deploying to gh-pages from @ yangxiaofeng/yangxiaofeng.github.io@f8e07ae42af483851e1e8e25173d575504a5f1ff 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -407,11 +407,11 @@ ninja.data = [{
           window.open("mailto:%78%69%61%6F%66%65%6E%67.%79%61%6E%67.%73%67@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-linkedin',
+        title: 'LinkedIn',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://www.linkedin.com/in/xiaofeng-yang-14891656", "_blank");
         },
       },{
         id: 'social-rss',
