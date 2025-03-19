2d09b95b87c97eaa4a370af9a3d9eb9c4aab8272
yangxiaofeng <xiaofeng.yang.sg@gmail.com>
2025-03-19 05:41:19 +0000

    Deploying to gh-pages from @ yangxiaofeng/yangxiaofeng.github.io@fee095b9fef662b37b5c892938127b30169c04f5 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -490,7 +490,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%78%69%61%6F%66%65%6E%67.%79%61%6E%67.%73%67@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
         id: 'social-inspire',
@@ -511,14 +511,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://scholar.google.com/citations?user=99ZjBGoAAAAJ", "_blank");
         },
       },{
       id: 'light-theme',
